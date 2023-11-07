59ee07cb11f47e1c72b1a9769279f270bab5b136
rmontenegroo <montenegro.r@gmail.com>
2023-11-07 20:04:23 -0300

    .

--- wifi-controller.py
+++ wifi-controller.py
@@ -1,264 +1,201 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import socket
 from os import system
+from threading import Thread, Semaphore
+from time import sleep
 
-#Definition of  motor pin 
-IN1 = 20
-IN2 = 21
-IN3 = 19
-IN4 = 26
-ENA = 16
-ENB = 13
-  
-UDP_IP = "0.0.0.0"
-UDP_PORT = 5050
-INITIAL_SPEED = 25
-MIN_SPEED = 5
-MAX_SPEED = 50
-DIFERENTIAL_FACTOR = 0.5
-
-#Set the GPIO port to BCM encoding mode
-GPIO.setmode(GPIO.BCM)
-
-#Ignore warning information
-GPIO.setwarnings(False)
-
-
-def decrease_speed():
-    global currentSpeed
-    global movementState
-
-    if currentSpeed - 1 >= MIN_SPEED:
-        currentSpeed -= 1
-
-    move()
+class PiTank(Thread):
     
-    print(f'Current speed: {currentSpeed}')
+    IN1 = 20
+    IN2 = 21
+    IN3 = 19
+    IN4 = 26
+    ENA = 16
+    ENB = 13    
 
+    FORWARD = 1
+    STOPPED = 0
+    BACKWARD = -1
+    ROTATERIGHT = 2
+    ROTATELEFT = -2
 
-def increase_speed():
-    global currentSpeed
-    global movementState
+    def __init__(self, UDP_IP = "0.0.0.0", UDP_PORT = 5050, sleepTime = 0.1, initialSpeed = 25, minSpeed = 5, maxSpeed = 50, diferentialFactor = 0.5, args = ..., kwargs = {}):
+        super().__init__(group=None, target=None, name=None, args=args, kwargs=kwargs, daemon=False)
 
-    if currentSpeed + 1 <= MAX_SPEED:
-        currentSpeed += 1
+        # GPIO.setmode(GPIO.BCM)
 
-    move()
+        # GPIO.setwarnings(False)
 
-    print(f'Current speed: {currentSpeed}')
-
-
-def leftSpeed():
-    global directionState 
-    global currentSpeed
-
-    if directionState == -1:
-        return currentSpeed
-    elif directionState == 1:
-        return round(currentSpeed * DIFERENTIAL_FACTOR)
-        
-    return currentSpeed
-
-
-def rightSpeed():
-    global directionState 
-    global currentSpeed
-
-    if directionState == 1:
-        return currentSpeed
-    elif directionState == -1:
-        return round(currentSpeed * DIFERENTIAL_FACTOR)
-        
-    return currentSpeed
-
-
-def move():
-    
-    global movementState
-    global pwm_ENA
-    global pwm_ENA
-
-    if movementState == 1:
-        # esta indo para frente
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.HIGH)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.HIGH) 
-
-    elif movementState == -1:
-        # esta indo para tras
-        GPIO.output(IN1, GPIO.HIGH)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW)
-
-    else:
-        # esta parado
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.LOW)
-
-
-    pwm_ENA.ChangeDutyCycle(rightSpeed())
-    pwm_ENB.ChangeDutyCycle(leftSpeed())
-
-
-def move_forward():
-        global movementState
-
-        movementState = 1
-
-        print("Robot Move Forward")
-        move()
-
-
-def move_backward():
-        global movementState
-
-        movementState = -1
-
-        print("Robot Move Backward")
-        move()
-
-
-def turn_left():
-    global directionState
-
-    directionState = -1
-
-    print('Robot turn left')
-    move()
-
-def turn_right():
-    global directionState
-
-    directionState = 1
-
-    print('Robot turn right')
-    move()
-
-
-def rotate_left():
-        global pwm_ENB
-        global pwm_ENA
-        global currentSpeed
-
-        GPIO.output(IN1, GPIO.HIGH)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.HIGH) 
-        pwm_ENA.ChangeDutyCycle(currentSpeed)
-        pwm_ENB.ChangeDutyCycle(currentSpeed)
-        print("Robot Rotate Right")
-        
-def rotate_right():
-        global pwm_ENB
-        global pwm_ENA
-        global currentSpeed
-
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.HIGH)
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW) 
-        pwm_ENA.ChangeDutyCycle(currentSpeed)
-        pwm_ENB.ChangeDutyCycle(currentSpeed)
-        print("Robot Rotate Left")
-
-def stop():
-        global pwm_ENB
-        global pwm_ENA
-        global currentSpeed
-        global movementState
-
-        movementState = 0
-
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.LOW)
-        pwm_ENA.ChangeDutyCycle(currentSpeed)
-        pwm_ENB.ChangeDutyCycle(currentSpeed)
-        print("Robot Stop")
-
-def init_speed():
-        global currentSpeed
-
-        currentSpeed = INITIAL_SPEED
-
-def init_motors():
-        global pwm_ENA
-        global pwm_ENB
-        global movementState
-        global directionState
-
-        directionState = 0
-        movementState = 0
-
-        GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
-        GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
-        GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
-        GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
-        GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
-        GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
+        # GPIO.setup(self.ENA,GPIO.OUT,initial=GPIO.HIGH)
+        # GPIO.setup(self.IN1,GPIO.OUT,initial=GPIO.LOW)
+        # GPIO.setup(self.IN2,GPIO.OUT,initial=GPIO.LOW)
+        # GPIO.setup(self.ENB,GPIO.OUT,initial=GPIO.HIGH)
+        # GPIO.setup(self.IN3,GPIO.OUT,initial=GPIO.LOW)
+        # GPIO.setup(self.IN4,GPIO.OUT,initial=GPIO.LOW)
             
-        #Set the PWM pin and frequency is 2000hz
-        pwm_ENA = GPIO.PWM(ENA, 2000)
-        pwm_ENB = GPIO.PWM(ENB, 2000)
-        pwm_ENA.start(0)
-        pwm_ENB.start(0)
+        # self.pwm_ENA = GPIO.PWM(self.ENA, 2000)
+        # self.pwm_ENB = GPIO.PWM(self.ENB, 2000)
+        # self.pwm_ENA.start(0)
+        # self.pwm_ENB.start(0)
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+        self.socket.settimeout(2)
+        self.socket.bind((UDP_IP, UDP_PORT))
+
+        self.currentSpeed = initialSpeed
+        self.diferentialFactor = diferentialFactor
+        self.minSpeed = minSpeed
+        self.maxSpeed = maxSpeed
+
+        self.sleepTime = sleepTime
+
+        # -1 left 0 straight 1 right
+        self.directionState = 0
+
+        # -1 backward 0 stopped 1 forward
+        self.movementState = 0
+
+        self.running = True
+
+        self.movementThread = Thread(target = self.move, daemon=True)
+        self.movementThread.start()
+
+        print('Running...')
 
 
-if __name__ == "__main__":
-        
-        global pwm_ENA
-        global pwm_ENB
-        global currentSpeed 
-        global movementState
-        global directionState
-        
-        init_speed()
-
-        init_motors()
-
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-        sock.bind((UDP_IP, UDP_PORT))
+    def move(self):
 
         while True:
-            raw, addr = sock.recvfrom(1024)
+            
+            if self.movementState == PiTank.FORWARD:
+                # esta indo para frente
+                # GPIO.output(IN1, GPIO.LOW)
+                # GPIO.output(IN2, GPIO.HIGH)
+                # GPIO.output(IN3, GPIO.LOW)
+                # GPIO.output(IN4, GPIO.HIGH)
+                print('Forward')
+
+            elif self.movementState == PiTank.BACKWARD:
+                # esta indo para tras
+                # GPIO.output(IN1, GPIO.HIGH)
+                # GPIO.output(IN2, GPIO.LOW)
+                # GPIO.output(IN3, GPIO.HIGH)
+                # GPIO.output(IN4, GPIO.LOW)
+                print('Backward')
+
+            elif self.movementState == PiTank.STOPPED:
+                # esta parado
+                # GPIO.output(IN1, GPIO.LOW)
+                # GPIO.output(IN2, GPIO.LOW)
+                # GPIO.output(IN3, GPIO.LOW)
+                # GPIO.output(IN4, GPIO.LOW)
+                print('Stopped')
+        
+            elif self.movementState == PiTank.ROTATERIGHT:
+                # GPIO.output(IN1, GPIO.LOW)
+                # GPIO.output(IN2, GPIO.HIGH)
+                # GPIO.output(IN3, GPIO.HIGH)
+                # GPIO.output(IN4, GPIO.LOW)
+                print('Rotate right')
+
+            elif self.movementState == PiTank.ROTATELEFT:
+                # GPIO.output(IN1, GPIO.HIGH)
+                # GPIO.output(IN2, GPIO.LOW)
+                # GPIO.output(IN3, GPIO.LOW)
+                # GPIO.output(IN4, GPIO.HIGH) 
+                print('Rotate left')
+
+            print(f"Speed: {self.leftSpeed()} {self.rightSpeed()}")
+
+            # self.pwm_ENA.ChangeDutyCycle(self.rightSpeed())
+            # self.pwm_ENB.ChangeDutyCycle(self.leftSpeed())
+
+            sleep(self.sleepTime)
+
+
+    def increaseSpeed(self):
+        if self.currentSpeed + 1 <= self.maxSpeed:
+            self.currentSpeed += 1
+
+
+    def decreaseSpeed(self):
+        if self.currentSpeed - 1 >= self.minSpeed:
+            self.currentSpeed -= 1
+
+
+    def rightSpeed(self):
+        if self.directionState == 1:
+            return self.currentSpeed * self.diferentialFactor
+        
+        return self.currentSpeed
+
+
+    def leftSpeed(self):
+        if self.directionState == -1:
+            return self.currentSpeed * self.diferentialFactor
+    
+        return self.currentSpeed
+
+
+    def turnRight(self):
+        if self.directionState + 1 <= 1:
+            self.directionState += 1
+
+
+    def turnLeft(self):
+        if self.directionState - 1 >= -1:
+            self.directionState -= 1
+
+
+    def run(self) -> None:
+
+        # GPIO.setmode(GPIO.BCM)
+        # GPIO.setwarnings(False)
+
+        while self.running:
+
+            try:
+                raw, addr = self.socket.recvfrom(1024)
+            except socket.timeout:
+                continue
 
             msg = raw.decode().lower()
 
             if msg == 'forward_pressed':
-                move_forward()
-          
+                self.movementState = 1
+                self.directionState = 0
+        
             elif msg == "stop_pressed":
-                stop()
+                self.movementState = 0
+                self.directionState = 0
 
             elif msg == "backward_pressed":
-                move_backward()
-                
-            elif msg == "right_pressed":
-                turn_right()
+                self.movementState = -1
+                self.directionState = 0
 
-            elif msg == "left_pressed":
-                turn_left()            
-
-            elif msg == "rotate_right_pressed":
-                rotate_right()
-
-            elif msg == "rotate_left_pressed":
-                rotate_left()
-            
             elif msg == "increase_speed":
-                increase_speed()
+                self.increaseSpeed()
 
             elif msg == "decrease_speed":
-                decrease_speed()
+                self.decreaseSpeed()
+                
+            elif msg == "right_pressed":
+                self.turnRight()
 
+            elif msg == "left_pressed":
+                self.turnLeft()
+
+            elif msg == "rotate_right_pressed":
+                self.movementState = 2
+                self.directionState = 0
+
+            elif msg == "rotate_left_pressed":
+                 self.movementState = -2
+                 self.directionState = 0
+            
             elif msg.startswith("testing:"):
                 print(msg, addr)
-                sock.sendto(raw, addr)
+                self.socket.sendto(raw, addr)
                 
             elif msg == "shutdown":
                 print("Shutdown")
@@ -267,4 +204,18 @@ if __name__ == "__main__":
             else:
                 print(msg, addr)
 
-sock.close()
+        self.socket.close()
+        print('Shutdown')
+
+
+if __name__ == "__main__":
+                
+    mainThread = PiTank()
+
+    try:
+        mainThread.start()
+        while mainThread.is_alive():
+            sleep(0.1)
+            
+    except KeyboardInterrupt:
+        mainThread.running = False
