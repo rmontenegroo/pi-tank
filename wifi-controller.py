d2cd5d437eef568decf35a0168814c3e5d6abd95
rmontenegroo <montenegro.r@gmail.com>
2023-11-07 14:30:28 -0300

    .

--- wifi-controller.py
+++ wifi-controller.py
@@ -1,118 +1,270 @@
 import RPi.GPIO as GPIO
 import socket
-import csv
+from os import system
 
-"""
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(33,GPIO.OUT)
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
-GPIO.setup(29,GPIO.OUT)
-GPIO.setup(31,GPIO.OUT)
-
-GPIO.output(29,True)
-GPIO.output(31,True)
-"""
-
+#Definition of  motor pin 
+IN1 = 20
+IN2 = 21
+IN3 = 19
+IN4 = 26
+ENA = 16
+ENB = 13
+  
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5050
+INITIAL_SPEED = 25
+MIN_SPEED = 5
+MAX_SPEED = 50
+DIFERENTIAL_FACTOR = 0.5
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-sock.bind((UDP_IP, UDP_PORT))
+#Set the GPIO port to BCM encoding mode
+GPIO.setmode(GPIO.BCM)
 
-while True:
-    raw, addr = sock.recvfrom(1024)
+#Ignore warning information
+GPIO.setwarnings(False)
 
-    msg = raw.decode().lower()
 
-    if msg == "forward_pressed":
-        """
-        GPIO.output(33,True)
-        GPIO.output(11,False)
-        GPIO.output(13,True)
-        GPIO.output(15,False)
-        """
-        print("Robot Move Forward")
-  
-  
-    elif msg == "stop_pressed":
-        """
-        GPIO.output(33,False)
-        GPIO.output(11,False)
-        GPIO.output(13,False)
-        GPIO.output(15,False)
-        """
-        print("Robot Stop")
+def decrease_speed():
+    global currentSpeed
+    global movementState
 
-    elif msg.endswith("_released"):
-        """
-        GPIO.output(33,False)
-        GPIO.output(11,False)
-        GPIO.output(13,False)
-        GPIO.output(15,False)
-        """
-        print("Robot Stop")
+    if currentSpeed - 1 >= MIN_SPEED:
+        currentSpeed -= 1
+
+    move()
     
+    print(f'Current speed: {currentSpeed}')
 
-    elif msg == "backward_pressed":
-        """
-        GPIO.output(33,False)
-        GPIO.output(11,True)
-        GPIO.output(13,False)
-        GPIO.output(15,True)
-        """
-        print("Robot Move Backward")
 
-    elif msg == "left_pressed":
-        """
-        GPIO.output(33,False)
-        GPIO.output(11,True)
-        GPIO.output(13,True)
-        GPIO.output(15,False)
-        """
-        print("Robot Move Left")
+def increase_speed():
+    global currentSpeed
+    global movementState
 
-    elif msg == "right_pressed":
-        """
-        GPIO.output(33,True)
-        GPIO.output(11,False)
-        GPIO.output(13,False)
-        GPIO.output(15,True)
-        """
-        print("Robot Move Right")
+    if currentSpeed + 1 <= MAX_SPEED:
+        currentSpeed += 1
 
-    elif msg == "rotate_right_pressed":
-        """
-        GPIO.output(33,True)
-        GPIO.output(11,False)
-        GPIO.output(13,False)
-        GPIO.output(15,True)
-        """
-        print("Robot Rotate Right")
+    move()
 
-    elif msg == "rotate_left_pressed":
-        """
-        GPIO.output(33,True)
-        GPIO.output(11,False)
-        GPIO.output(13,False)
-        GPIO.output(15,True)
-        """
-        print("Robot Rotate Left")
+    print(f'Current speed: {currentSpeed}')
 
-    elif msg.startswith("testing:"):
-        print(msg, addr)
-        sock.sendto(raw, addr)
+
+def leftSpeed():
+    global directionState 
+    global currentSpeed
+
+    if directionState == -1:
+        return currentSpeed
+    elif directionState == 1:
+        return round(currentSpeed * DIFERENTIAL_FACTOR)
+        
+    return currentSpeed
+
+
+def rightSpeed():
+    global directionState 
+    global currentSpeed
+
+    if directionState == 1:
+        return currentSpeed
+    elif directionState == -1:
+        return round(currentSpeed * DIFERENTIAL_FACTOR)
+        
+    return currentSpeed
+
+
+def move():
+    
+    global movementState
+    global pwm_ENA
+    global pwm_ENA
+
+    if movementState == 1:
+        # esta indo para frente
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.HIGH) 
+
+    elif movementState == -1:
+        # esta indo para tras
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.HIGH)
+        GPIO.output(IN4, GPIO.LOW)
 
     else:
-        print(msg, addr)
-        """
-        GPIO.output(33,False)
-        GPIO.output(11,False)
-        GPIO.output(13,False)
-        GPIO.output(15,False)
-        """
-        # print("STOP")
+        # esta parado
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.LOW)
+
+
+    pwm_ENA.ChangeDutyCycle(rightSpeed())
+    pwm_ENB.ChangeDutyCycle(leftSpeed())
+
+
+def move_forward():
+        global movementState
+
+        movementState = 1
+
+        print("Robot Move Forward")
+        move()
+
+
+def move_backward():
+        global movementState
+
+        movementState = -1
+
+        print("Robot Move Backward")
+        move()
+
+
+def turn_left():
+    global directionState
+
+    directionState = -1
+
+    print('Robot turn left')
+    move()
+
+def turn_right():
+    global directionState
+
+    directionState = 1
+
+    print('Robot turn right')
+    move()
+
+
+def rotate_left():
+        global pwm_ENB
+        global pwm_ENA
+        global currentSpeed
+
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.HIGH) 
+        pwm_ENA.ChangeDutyCycle(currentSpeed)
+        pwm_ENB.ChangeDutyCycle(currentSpeed)
+        print("Robot Rotate Right")
+        
+def rotate_right():
+        global pwm_ENB
+        global pwm_ENA
+        global currentSpeed
+
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+        GPIO.output(IN3, GPIO.HIGH)
+        GPIO.output(IN4, GPIO.LOW) 
+        pwm_ENA.ChangeDutyCycle(currentSpeed)
+        pwm_ENB.ChangeDutyCycle(currentSpeed)
+        print("Robot Rotate Left")
+
+def stop():
+        global pwm_ENB
+        global pwm_ENA
+        global currentSpeed
+        global movementState
+
+        movementState = 0
+
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.LOW)
+        pwm_ENA.ChangeDutyCycle(currentSpeed)
+        pwm_ENB.ChangeDutyCycle(currentSpeed)
+        print("Robot Stop")
+
+def init_speed():
+        global currentSpeed
+
+        currentSpeed = INITIAL_SPEED
+
+def init_motors():
+        global pwm_ENA
+        global pwm_ENB
+        global movementState
+        global directionState
+
+        directionState = 0
+        movementState = 0
+
+        GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
+        GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
+        GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
+            
+        #Set the PWM pin and frequency is 2000hz
+        pwm_ENA = GPIO.PWM(ENA, 2000)
+        pwm_ENB = GPIO.PWM(ENB, 2000)
+        pwm_ENA.start(0)
+        pwm_ENB.start(0)
+
+
+if __name__ == "__main__":
+        
+        global pwm_ENA
+        global pwm_ENB
+        global currentSpeed 
+        global movementState
+        global directionState
+        
+        init_speed()
+
+        init_motors()
+
+        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+        sock.bind((UDP_IP, UDP_PORT))
+
+        while True:
+            raw, addr = sock.recvfrom(1024)
+
+            msg = raw.decode().lower()
+
+            if msg == 'forward_pressed':
+                move_forward()
+          
+            elif msg == "stop_pressed":
+                stop()
+
+            elif msg == "backward_pressed":
+                move_backward()
+                
+            elif msg == "right_pressed":
+                turn_right()
+
+            elif msg == "left_pressed":
+                turn_left()            
+
+            elif msg == "rotate_right_pressed":
+                rotate_right()
+
+            elif msg == "rotate_left_pressed":
+                rotate_left()
+            
+            elif msg == "increase_speed":
+                increase_speed()
+
+            elif msg == "decrease_speed":
+                decrease_speed()
+
+            elif msg.startswith("testing:"):
+                print(msg, addr)
+                sock.sendto(raw, addr)
+                
+            elif msg == "shutdown":
+                print("Shutdown")
+                system("sudo shutdown -h now")                
+
+            else:
+                print(msg, addr)
 
 sock.close()
