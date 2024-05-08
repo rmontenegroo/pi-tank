c454f6a3b36da416e1f859b41efa963f5fa18242
rmontenegroo <montenegro.r@gmail.com>
2024-05-08 10:37:18 -0300

    .

--- test_ultrasonic.py
+++ test_ultrasonic.py
@@ -1,76 +1,21 @@
-#-*- coding:UTF-8 -*-
-import RPi.GPIO as GPIO
-import time
+from gpiozero import DistanceSensor
 
-EchoPin = 0
-TrigPin = 1
+us_sensor = DistanceSensor(echo=0, trigger=1, threshold_distance=0.1, max_distance=2)
 
-GPIO.setmode(GPIO.BCM)
+def in_range():
+    print(f'In range {us_sensor.distance}')
 
-GPIO.setwarnings(False)
+def out_of_range():
+    print(f'Out of range {us_sensor.distance}')
 
-def init():
-    GPIO.setup(EchoPin,GPIO.IN)
-    GPIO.setup(TrigPin,GPIO.OUT)
-    print('init done')
+us_sensor.when_in_range = in_range
+us_sensor.when_out_of_range = out_of_range
 
+# print(dir(us_sensor))
 
-def Distance():
-    GPIO.output(TrigPin,GPIO.LOW)
-    time.sleep(0.000002)
-    GPIO.output(TrigPin,GPIO.HIGH)
-    time.sleep(0.000015)
-    GPIO.output(TrigPin,GPIO.LOW)
-
-    t3 = time.time()
-
-    while not GPIO.input(EchoPin):
-        t4 = time.time()
-        if (t4 - t3) > 0.03 :
-            return -1
-
-
-    t1 = time.time()
-    while GPIO.input(EchoPin):
-        t5 = time.time()
-        if(t5 - t1) > 0.03 :
-            return -1
-
-    t2 = time.time()
-    time.sleep(0.01)
-#    print "distance is %d " % (((t2 - t1)* 340 / 2) * 100)
-    return ((t2 - t1)* 340 / 2) * 100
-
-def Distance_test():
-    num = 0
-    ultrasonic = []
-    while num < 5:
-            distance = Distance()
-            while int(distance) == -1 :
-                distance = Distance()
-                print("Tdistance is %f"%(distance) )
-            while (int(distance) >= 500 or int(distance) == 0) :
-                distance = Distance()
-                print("Edistance is %f"%(distance) )
-            ultrasonic.append(distance)
-            num = num + 1
-            time.sleep(0.01)
-    print(ultrasonic)
-    distance = (ultrasonic[1] + ultrasonic[2] + ultrasonic[3])/3
-    print("distance is %f"%(distance) ) 
-    return distance
-
-
-time.sleep(2)
-
-try:
-    init()
-    while True:
-        distance = Distance_test()
-        time.sleep(0.001)
-       
-except KeyboardInterrupt:
-    pass
-
-GPIO.cleanup()
-
+while True:
+    # print(f'Distance: {us_sensor.distance}')
+    us_sensor.wait_for_in_range()
+    # print(f'In range {us_sensor.distance}')
+    us_sensor.wait_for_out_of_range()
+    # print(f'Out of range {us_sensor.distance}')
